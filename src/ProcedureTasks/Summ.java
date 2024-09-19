f6f0902bb7e510394f93b9524c68932349f2e8ac
HOME-PC\Илья <iluxx_ivanov@mail.ru>
2024-09-19 22:05:06 +0400

    Added the homework1 (summ of args)

---
+++ src/ProcedureTasks/Summ.java
@@ -0,0 +1,15 @@
+package ProcedureTasks;
+
+public class Summ
+{
+    public static void main(String[] args)
+    {
+        int result = 0;
+
+        for (int i=0; i< args.length; i++)
+        {
+            result += Integer.parseInt(args[i]);
+        }
+        System.out.printf("Result = %d", result);
+    }
+}
